ff6686428faed97a8730c256e5ffccf2917f3262
Ian Ngulube <ngulubeian94@gmail.com>
2024-07-04 19:29:18 +0200

    refactor:api:add UssdApi class

---
+++ src/main/java/za/co/mafsoft/mtn/rsa/ussd/api/UssdApi.java
@@ -0,0 +1,22 @@
+package za.co.mafsoft.mtn.rsa.ussd.api;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+@Path("/mtn/zam/ussd")
+public class UssdApi {
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Object invoke(Object request) {
+        log.info("Received USSD request: {}", request);
+        return List.of(request);
+    }
+}
